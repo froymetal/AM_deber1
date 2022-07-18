4b485bdbc146f3d26cc07bb28cd1ed74f3378960
Froy <54004493+froymetal@users.noreply.github.com>
2022-07-18 07:46:01 -0500

    Creating segment options

--- AM_deber1/ViewController.swift
+++ AM_deber1/ViewController.swift
@@ -20,13 +20,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fechaLabel.text = getDate()
+        generateTip(servicioSegment as Any)
     }
 
-    func generateTip() {
-        
+    @IBAction func generateTip(_ sender: Any) {
+        let selectedindex = servicioSegment.selectedSegmentIndex
+
+        switch selectedindex {
+        case 0:
+            print("vallet")
+        case 1:
+            print("limpieza")
+        case 2:
+            print("conserje")
+        default:
+            print("error")
+        }
     }
+
     @IBAction func payTip(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "DeLuxe Grand Hotel",
+            message: "Gracias por su pago",
+            preferredStyle: .alert)
+        let btn = UIAlertAction(title: "Cerrar", style: .default, handler: nil)
+        alert.addAction(btn)
+        self.present(alert, animated: true)
     }
+
     @IBAction func cancelAndRestart(_ sender: Any) {
 
     }
