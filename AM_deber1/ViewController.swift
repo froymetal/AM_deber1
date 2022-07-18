dcb5eb2c21fbbc971f9e7d41be43a58f950df3e7
Froy <54004493+froymetal@users.noreply.github.com>
2022-07-17 22:19:21 -0500

    Creating outlets

--- AM_deber1/ViewController.swift
+++ AM_deber1/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK - OUTLETS
+    @IBOutlet weak var montoTotalLabel: UILabel!
+    @IBOutlet weak var servicioSegment: UISegmentedControl!
+    @IBOutlet weak var fechaLabel: UILabel!
+    @IBOutlet weak var montoTipLabel: UILabel!
+    @IBOutlet weak var tipButton: UIButton!
+
+    // MARK - Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fechaLabel.text = getDate()
     }
 
+    func generateTip() {
+        
+    }
+    @IBAction func payTip(_ sender: Any) {
+    }
+    @IBAction func cancelAndRestart(_ sender: Any) {
 
+    }
+
+    func getDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let str = dateFormatter.string(from: date)
+        return str
+    }
 }
 
